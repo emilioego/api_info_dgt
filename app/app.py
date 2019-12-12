39de32c77f991879a5bdd261aba6cb7aebf84e47
Lucia <lucfalgar@gmail.com>
2019-12-12 20:50:48 +0100

    Añadido nuevos métodos: GET,POST,DELETE y atributo id_conductor cambiado por dni

--- app/app.py
+++ app/app.py
@@ -56,44 +56,44 @@ class Puntos(Resource):
     def get(self):
         output = []
         for s in db.test.find():
-            output.append({'puntos_actuales' : s['puntos_actuales'], 'puntos_perdidos' : s['puntos_perdidos'],'id_conductor' : s['id_conductor'],'puntos_recuperados' : s['puntos_recuperados'],'date' : s['date']})
+            output.append({'puntos_actuales' : s['puntos_actuales'], 'puntos_perdidos' : s['puntos_perdidos'],'dni' : s['dni'],'puntos_recuperados' : s['puntos_recuperados'],'date' : s['date']})
         return jsonify({'result' : output})
 
     # Inserta un nuevo conductor 
     def post(self):
-        id_conductor = request.json['id_conductor']
+        dni = request.json['dni']
         puntos_actuales = request.json['puntos_actuales']
         puntos_perdidos = request.json['puntos_perdidos']
         puntos_recuperados = request.json['puntos_recuperados']
         timestamp=datetime.utcnow()
-        nuevo_id = db.test.insert({'id_conductor': id_conductor,'puntos_actuales': puntos_actuales, 'puntos_perdidos': puntos_perdidos, 'puntos_recuperados': puntos_recuperados, 'date' : timestamp })
+        nuevo_id = db.test.insert({'dni': dni,'puntos_actuales': puntos_actuales, 'puntos_perdidos': puntos_perdidos, 'puntos_recuperados': puntos_recuperados, 'date' : timestamp })
         nuevo_registro = db.test.find_one({'_id': nuevo_id })
-        output = { 'id_conductor' : nuevo_registro['id_conductor'],'puntos_actuales' : nuevo_registro['puntos_actuales'], 'puntos_perdidos' : nuevo_registro['puntos_perdidos'], 'puntos_recuperados' : nuevo_registro['puntos_recuperados'],'date' : nuevo_registro['date']}
+        output = { 'dni' : nuevo_registro['dni'],'puntos_actuales' : nuevo_registro['puntos_actuales'], 'puntos_perdidos' : nuevo_registro['puntos_perdidos'], 'puntos_recuperados' : nuevo_registro['puntos_recuperados'],'date' : nuevo_registro['date']}
         return jsonify({'result' : output})
 
     #Borra del sistema los puntos de un conductor específico
     def delete(self):
-        id_conductor = flask.request.args.get("id_conductor") 
-        db.test.delete_one({'id_conductor' : id_conductor })
+        dni = flask.request.args.get("dni") 
+        db.test.delete_one({'dni' : dni })
         return "Registro borrado correctamente"
        
 
 class PuntosConductor(Resource):
     #Trae la información de los puntos de un conductor en concreto
-    def get(self,id_conductor):
+    def get(self,dni):
         output = []
-        for s in db.test.find({"id_conductor":id_conductor}):
-            output.append({'puntos_actuales' : s['puntos_actuales'], 'puntos_perdidos' : s['puntos_perdidos'],'id_conductor' : s['id_conductor'],'puntos_recuperados' : s['puntos_recuperados'],'date' : s['date']})
+        for s in db.test.find({"dni":dni}):
+            output.append({'puntos_actuales' : s['puntos_actuales'], 'puntos_perdidos' : s['puntos_perdidos'],'dni' : s['dni'],'puntos_recuperados' : s['puntos_recuperados'],'date' : s['date']})
         return jsonify({'result' : output})
 
 class Historial(Resource):
     def get(self,dni):
-        records = [doc for doc in db.test.find({"id_conductor":dni})]
+        records = [doc for doc in db.test.find({"dni":dni})]
         return json.loads(json_util.dumps(records))
 
 class Multa(Resource):
     def get(self,dni):
-        records = [doc for doc in db.test.find({"id_conductor":dni})]
+        records = [doc for doc in db.test.find({"dni":dni})]
         return json.loads(json_util.dumps(records))
 
 # =========================
@@ -101,7 +101,7 @@ class Multa(Resource):
 # =========================
 api.add_resource(Historial,'/puntos/historial/<dni>')
 api.add_resource(Puntos,'/puntos')
-api.add_resource(PuntosConductor,'/puntos/<id_conductor>')
+api.add_resource(PuntosConductor,'/puntos/<dni>')
 
 if __name__ == '__main__':
     app.run(debug=True)
