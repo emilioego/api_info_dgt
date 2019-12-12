8136a7fe60ca08a0aa44d0bd02991d56efe8a9d1
Lucia <lucfalgar@gmail.com>
2019-12-12 20:34:31 +0100

    Añadido nuevos métodos: GET,POST,DELETE

--- app/app.py
+++ app/app.py
@@ -8,9 +8,10 @@ from flask import Flask,request, jsonify
 from flask_restplus import Resource, Api
 from pymongo import MongoClient
 import urllib.parse
-import datetime
+from datetime import datetime
 import json
 from bson import json_util, ObjectId
+import flask
 
 # =========================
 # Extensions initialization
@@ -43,13 +44,47 @@ def validoDNI(dni_min):
 # Clases
 # =========================
 
-class Puntos():
+class Puntos(Resource):
     def __init__(self,dni):
         self.puntos_actuales = 13
         self.dni = dni
         self.puntos_perdidos = 0
         self.puntos_recuperados = 0
-        self.timestamp = datetime.datetime.utcnow()
+        self.timestamp = datetime.utcnow()
+    
+    #Se obtiene toda la información de los puntos
+    def get(self):
+        output = []
+        for s in db.test.find():
+            output.append({'puntos_actuales' : s['puntos_actuales'], 'puntos_perdidos' : s['puntos_perdidos'],'id_conductor' : s['id_conductor'],'puntos_recuperados' : s['puntos_recuperados'],'date' : s['date']})
+        return jsonify({'result' : output})
+
+    # Inserta un nuevo conductor 
+    def post(self):
+        id_conductor = request.json['id_conductor']
+        puntos_actuales = request.json['puntos_actuales']
+        puntos_perdidos = request.json['puntos_perdidos']
+        puntos_recuperados = request.json['puntos_recuperados']
+        timestamp=datetime.utcnow()
+        nuevo_id = db.test.insert({'id_conductor': id_conductor,'puntos_actuales': puntos_actuales, 'puntos_perdidos': puntos_perdidos, 'puntos_recuperados': puntos_recuperados, 'date' : timestamp })
+        nuevo_registro = db.test.find_one({'_id': nuevo_id })
+        output = { 'id_conductor' : nuevo_registro['id_conductor'],'puntos_actuales' : nuevo_registro['puntos_actuales'], 'puntos_perdidos' : nuevo_registro['puntos_perdidos'], 'puntos_recuperados' : nuevo_registro['puntos_recuperados'],'date' : nuevo_registro['date']}
+        return jsonify({'result' : output})
+
+    #Borra del sistema los puntos de un conductor específico
+    def delete(self):
+        id_conductor = flask.request.args.get("id_conductor") 
+        db.test.delete_one({'id_conductor' : id_conductor })
+        return "Registro borrado correctamente"
+       
+
+class PuntosConductor(Resource):
+    #Trae la información de los puntos de un conductor en concreto
+    def get(self,id_conductor):
+        output = []
+        for s in db.test.find({"id_conductor":id_conductor}):
+            output.append({'puntos_actuales' : s['puntos_actuales'], 'puntos_perdidos' : s['puntos_perdidos'],'id_conductor' : s['id_conductor'],'puntos_recuperados' : s['puntos_recuperados'],'date' : s['date']})
+        return jsonify({'result' : output})
 
 class Historial(Resource):
     def get(self,dni):
@@ -66,7 +101,7 @@ class Multa(Resource):
 # =========================
 api.add_resource(Historial,'/puntos/historial/<dni>')
 api.add_resource(Puntos,'/puntos')
-
+api.add_resource(PuntosConductor,'/puntos/<id_conductor>')
 
 if __name__ == '__main__':
     app.run(debug=True)
