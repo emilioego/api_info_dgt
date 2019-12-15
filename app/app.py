d5c4e8107fc9b594b9a0cc2979de8c7567442712
emilioego <emilioego@hotmail.com>
2019-12-15 19:57:18 +0100

    Se quita el valida dni ya que no lo vamos a utilizar

--- app/app.py
+++ app/app.py
@@ -25,21 +25,6 @@ db = client['puntos']
 # Metodos
 # =========================
 
-def validoDNI(dni_min):
-    tabla = "TRWAGMYFPDXBNJZSQVHLCKE"
-    dig_ext = "XYZ"
-    reemp_dig_ext = {'X':'0', 'Y':'1', 'Z':'2'}
-    numeros = "1234567890"
-    dni = dni_min.upper()
-    if len(dni) == 9:
-        dig_control = dni[8]
-        dni = dni[:8]
-        if dni[0] in dig_ext:
-            dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
-        if len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni)%23] == dig_control:
-            return dni_min
-    return False
-
 # =========================
 # Clases
 # =========================
