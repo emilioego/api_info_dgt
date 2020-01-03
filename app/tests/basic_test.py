c56a39867e9c88633f03140de183d8577b13148d
emilioego <emilioego@hotmail.com>
2020-01-04 00:46:20 +0100

    Se aladen varias pruebas

--- app/tests/basic_test.py
+++ app/tests/basic_test.py
@@ -3,6 +3,7 @@ import os.path
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from app import app
+import flask_api.request
 from flask import json
 
 def func(x):
@@ -11,6 +12,30 @@ def func(x):
 def test_answer():
     assert func(3) == 4
 
-def test_get():
-    response = app.test_client().get('/puntos')
+def test_get_not_auth():
+    client = app.test_client()
+    url = '/puntos'
+    response = client.get(url)
+    assert response.status_code == 404
+
+def test_get_auth():
+    client = app.test_client()
+    url = '/puntos'
+    headers = {
+            'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'
+        }
+    response = client.get(url, headers=headers)
     assert response.status_code == 200
+
+def test_post_route__success():
+    client = app.test_client()
+    url = '/puntos'
+    mock_request_headers = {
+        'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'
+    }
+    mock_request_data = {
+        'dni': '20067771F',
+    }
+
+    response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
+    assert response.status_code == 201
