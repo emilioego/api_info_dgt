fc5f31562e94c34370f7abfe2a7c83d43e8de9bd
emilioego <emilioego@hotmail.com>
2020-01-05 11:42:47 +0100

    Se modifican los test

--- app/tests/basic_test.py
+++ app/tests/basic_test.py
@@ -4,7 +4,7 @@ sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from app import app
 import flask_api.request
-from flask import json
+from flask import json, Flask
 
 def func(x):
     return x + 1
@@ -13,12 +13,16 @@ def test_answer():
     assert func(3) == 4
 
 def test_get_not_auth():
+    app = Flask(__name__)
+    app(app)
     client = app.test_client()
     url = '/puntos'
     response = client.get(url)
     assert response.status_code == 404
 
 def test_get_auth():
+    app = Flask(__name__)
+    app(app)
     client = app.test_client()
     url = '/puntos'
     headers = {
@@ -28,6 +32,8 @@ def test_get_auth():
     assert response.status_code == 200
 
 def test_post_route__success():
+    app = Flask(__name__)
+    app(app)    
     client = app.test_client()
     url = '/puntos'
     mock_request_headers = {
