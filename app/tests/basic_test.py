23729c932cb780da401b54eb7fb4a09aa17d4cc8
emilioego <emilioego@hotmail.com>
2020-01-07 19:41:27 +0100

    Se añaden mas pruebas

--- app/tests/basic_test.py
+++ app/tests/basic_test.py
@@ -6,6 +6,42 @@ from app import app
 import flask_api.request
 from flask import json, Flask
 
+def test_multa_puntos():
+    client = app.test_client()
+    url = '/api/v1/puntos/20154021M/multa?npuntos=5'
+    headers = {
+            'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'
+        }
+    response = client.post(url, headers=headers)
+    assert response.status_code == 201
+
+def test_recupera_puntos():
+    client = app.test_client()
+    url = '/api/v1/puntos/20154021M/recupera?npuntos=5'
+    headers = {
+            'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'
+        }
+    response = client.post(url, headers=headers)
+    assert response.status_code == 201
+
+def test_get_history_error():
+    client = app.test_client()
+    url = '/api/v1/puntos/historial/201540211M'
+    headers = {
+            'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'
+        }
+    response = client.get(url, headers=headers)
+    assert response.status_code == 404
+
+def test_get_history():
+    client = app.test_client()
+    url = '/api/v1/puntos/historial/20154021M'
+    headers = {
+            'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'
+        }
+    response = client.get(url, headers=headers)
+    assert response.status_code == 200
+
 def test_get_not_auth():
     client = app.test_client()
     url = '/puntos'
@@ -21,7 +57,7 @@ def test_get_auth():
     response = client.get(url, headers=headers)
     assert response.status_code == 200
 
-def test_post_route__success(): 
+def test_post_route_success(): 
     client = app.test_client()
     url = '/api/v1/puntos'
     mock_request_headers = {
@@ -45,24 +81,6 @@ def test_put_route_success():
     response = client.put(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
     print(response)
     assert response.status_code == 200
-    
-def get_history_success(): 
-    client = app.test_client()
-    url = '/api/v1/puntos/historial/20067771F'
-    mock_request_headers = {
-        'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'
-    }
-    response = client.delete(url, headers=mock_request_headers)
-    assert response.status_code == 200
-
-def get_history_error(): 
-    client = app.test_client()
-    url = '/api/v1/puntos/historial/20063371F'
-    mock_request_headers = {
-        'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'
-    }
-    response = client.delete(url, headers=mock_request_headers)
-    assert response.status_code == 404
 
 def test_delete_route_success(): 
     client = app.test_client()
